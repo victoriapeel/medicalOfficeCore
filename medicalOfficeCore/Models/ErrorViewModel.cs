0773eb073148e8aa481a742c4938a853c645ebfd
lil Rae <victoriapeel23@outlook.com>
2018-09-10 08:45:13 -0400

    Add project files.

---
+++ medicalOfficeCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace medicalOfficeCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
